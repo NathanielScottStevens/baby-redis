24d726de011584a4edb4ea073feea21cba74d1a5
Nathaniel Stevens <nathaniel.scott.stevens@gmail.com>
2020-10-23 14:32:02 -0400

    Adds integers and lists to serialization

--- lib/baby_redis/serializer.ex
+++ lib/baby_redis/serializer.ex
@@ -13,11 +13,64 @@ defmodule BabyRedis.Serializer do
     {:ok, ":#{data}\r\n"}
   end
 
+  def serialize(data) when is_list(data) do
+    length = Enum.count(data)
+
+    # TODO handle errors in serializing items
+    serialized_data = data |> Enum.map(&serialize!/1) |> Enum.join()
+
+    {:ok, "*#{length}\r\n#{serialized_data}"}
+  end
+
+  def serialize(_data), do: {:error, :unknown_type}
+
+  @spec serialize!(String.t()) :: String.t()
+  def serialize!(data) do
+    {:ok, serialized_data} = serialize(data)
+    serialized_data
+  end
+
   @doc """
     Deserializes data from redis protocol
   """
   @spec deserialize(String.t()) :: {:ok, String.t()} | {:error, atom()}
-  def deserialize(_data) do
-    {:error, :not_implemented}
+  def deserialize("+" <> data) when is_binary(data) do
+    {:ok, String.trim(data)}
+  end
+
+  def deserialize(":" <> data) when is_binary(data) do
+    value = String.trim(data)
+
+    with {int, _} <- Integer.parse(value) do
+      {:ok, int}
+    else
+      _ -> {:error, :not_an_integer}
+    end
+  end
+
+  def deserialize("*" <> data) when is_binary(data) do
+    [length | items] =
+      data
+      |> String.split("\r\n")
+      |> Enum.map(&String.trim/1)
+      |> Enum.reject(&(&1 == ""))
+
+    # TODO handle errors in deserializing items
+    deserialized_items = Enum.map(items, &deserialize!/1)
+    {parsed_length, _} = Integer.parse(length)
+
+    if parsed_length == Enum.count(deserialized_items) do
+      {:ok, deserialized_items}
+    else
+      {:error, :unmatched_items}
+    end
+  end
+
+  def deserialize(_data), do: {:error, :not_a_valid_type}
+
+  @spec deserialize!(String.t()) :: String.t()
+  def deserialize!(data) do
+    {:ok, deserialized_data} = deserialize(data)
+    deserialized_data
   end
 end
--- test/baby_redis/serializer_test.exs
+++ test/baby_redis/serializer_test.exs
@@ -3,12 +3,47 @@ defmodule SerializerTest do
   alias BabyRedis.Serializer
 
   describe "serialize" do
-    test "serializes strings" do
+    test "strings" do
       assert {:ok, "+A String\r\n"} == Serializer.serialize("A String")
     end
 
-    test "serializes integers" do
+    test "integers" do
       assert {:ok, ":555\r\n"} == Serializer.serialize(555)
+    end
+
+    test "list" do
+      assert {:ok, "*2\r\n+hello\r\n:555\r\n"} == Serializer.serialize(["hello", 555])
+    end
+
+    test "unknown type returns error" do
+      assert {:error, :unknown_type} == Serializer.serialize(:atom)
+    end
+  end
+
+  describe "deserialize" do
+    test "strings" do
+      assert {:ok, "A String"} == Serializer.deserialize("+A String\r\n")
+    end
+
+    test "integers" do
+      assert {:ok, 555} == Serializer.deserialize(":555\r\n")
+    end
+
+    test "list" do
+      assert {:ok, ["hello", 555]} == Serializer.deserialize("*2\r\n+hello\r\n:555\r\n")
+    end
+
+    test "returns error if list has too few items" do
+      assert {:error, :unmatched_items} ==
+               Serializer.deserialize("*3\r\n+hello\r\n:555\r\n")
+    end
+
+    test "returns error if not an integer" do
+      assert {:error, :not_an_integer} == Serializer.deserialize(":a_word\r\n")
+    end
+
+    test "returns error if a nonvalid type prefix" do
+      assert {:error, :not_a_valid_type} == Serializer.deserialize("~a_word\r\n")
     end
   end
 end
